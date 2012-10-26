84b5e8652d56809d3011fb8274d0cd816b79bb59
blake <blake@blake-ubu.(none)>
2012-10-26 13:32:33 +1100

    fixed syntax error

--- src/EchoOutputHandler.php
+++ src/EchoOutputHandler.php
@@ -9,6 +9,6 @@ class EchoOutputHandler implements OutputHandler
     {
         echo ($this->colours ? "\033[1;32m" : '').date('Y-m-d H:i').': '.$job->raw.PHP_EOL;
         $output = trim($output);
-        echo ($this->colours ? "\033[0m").($output ? $output.PHP_EOL.PHP_EOL : null);
+        echo ($this->colours ? "\033[0m" : '').($output ? $output.PHP_EOL.PHP_EOL : null);
     }
 }

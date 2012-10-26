33337ba1d394da593c41d4209705869676b2da1a
blake <blake@blake-ubu.(none)>
2012-10-26 12:33:12 +1100

    added colours to EchoOutputHandler

--- src/EchoOutputHandler.php
+++ src/EchoOutputHandler.php
@@ -3,10 +3,12 @@ namespace Phonycron;
 
 class EchoOutputHandler implements OutputHandler
 {
+    public $colours = false;
+
     public function handle(Job $job, $output)
     {
-        echo date('Y-m-d H:i').': '.$job->raw.PHP_EOL;
+        echo ($this->colours ? "\033[1;32m" : '').date('Y-m-d H:i').': '.$job->raw.PHP_EOL;
         $output = trim($output);
-        echo $output ? $output.PHP_EOL.PHP_EOL : null;
+        echo ($this->colours ? "\033[0m").($output ? $output.PHP_EOL.PHP_EOL : null);
     }
 }
